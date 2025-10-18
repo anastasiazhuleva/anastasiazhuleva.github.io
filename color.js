d7456f68b682a7a68068ab84107a76f15cc104a6
nastyamakli <nastyazhul2006@gmail.com>
2025-10-18 05:20:16 +0000

    Sat, Oct 18, 2025, 12:20 PM +07:00

---
+++ color.js
@@ -0,0 +1,8 @@
+document.addEventListener ("DOMContentLoaded", function(
+    {
+        let body = document.querySelector("body");
+        document.querySelector("#red").addEventListener("click",
+            function(){ body.stylebackgroundColor = }
+        )
+    }
+))
